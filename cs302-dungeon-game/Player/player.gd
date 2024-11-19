2a6070f94731ef852426b3c4e940b98e539c5d3d
rwolpert8 <rwolpert@vols.utk.edu>
2024-11-19 18:29:29 -0500

    Update player.gd

--- cs302-dungeon-game/Player/player.gd
+++ cs302-dungeon-game/Player/player.gd
@@ -5,9 +5,21 @@ const max_speed = 10
 #position of the CharacterBody2D object, which is the player
 var weapon_direction = get_global_mouse_position() - global_position
 var input = Vector2.ZERO
+var bullet = preload("res://Bullet/player_bullet1.tscn")
+var bullet_speed = 100
 
 func _physics_process(delta):
 	move()
+	
+func _shoot():
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("ShootBullet"):
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = $BulletPoint.get_global_position()
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
+		
 	
 func get_XandY():
 	#x position is just dependent on right and left subtractive movements. Same with y position, just up and down
